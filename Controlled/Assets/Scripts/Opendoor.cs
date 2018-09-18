177dc066e8f4ab1335c16e92e2a7bac2b097242f
Cam Jones <camo080196@gmail.com>
2018-09-18 16:41:04 +0200

    2nd Commit
    2nd Commit

---
+++ Controlled/Assets/Scripts/Opendoor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Opendoor : MonoBehaviour {
+
+    Animator anim;
+    BoxCollider2D door;
+
+    void Start()
+    {
+        anim = GameObject.FindGameObjectWithTag("Door1").GetComponent<Animator>();
+        door = GameObject.FindGameObjectWithTag("Door1").GetComponent<BoxCollider2D>();
+    }
+
+    
+    void Update () {
+		
+	}
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        
+        if (collision.gameObject.tag == "Player")
+        {
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("Opening");
+                anim.SetBool("Closed", false);
+                anim.SetBool("Opened", true);
+                door.enabled = false;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            Debug.Log("Closing");
+            anim.SetBool("Opened", false);
+            anim.SetBool("Closed", true);
+            door.enabled = true;
+        }
+    }
+}
--- Controlled/Assets/Scripts/PlayerController.cs
+++ Controlled/Assets/Scripts/PlayerController.cs
@@ -7,12 +7,103 @@ public class PlayerController : MonoBehaviour {
     [SerializeField]
     private float speed;
 
+    private Animator anim;
+    private static PlayerController instance = null;
+
+    [SerializeField] private Rigidbody2D player;
+
+    private bool forward = true;
+
+    public static PlayerController Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    public bool Forward
+    {
+        get
+        {
+            return forward;
+        }
+
+        set
+        {
+            forward = value;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
     private void FixedUpdate()
     {
-        float moveHoriz = Input.GetAxis("Horizontal");
-        float moveVert = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(moveHoriz, moveVert, 0);
-        transform.position += movement * speed * Time.deltaTime;
+        ResetValues();
+        HandleInput();
         
+    }
+
+    private void HandleInput()
+    {
+        FlipPlayer();
+
+        if (Input.GetKey("d"))
+        {
+            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+            anim.SetBool("Move", true);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
+            anim.SetBool("Move", true);
+        }
+
+        if (Input.GetKey("w"))
+        {
+            transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+            anim.SetBool("Move", true);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            transform.position -= new Vector3(0, speed * Time.deltaTime, 0);
+            anim.SetBool("Move", true);
+        }
+    }
+
+    void FlipPlayer()
+    {
+        float movement = Input.GetAxis("Horizontal");
+
+        if ((movement > 0 && !Forward) || (movement < 0 && Forward))
+        {
+            Vector3 playerScale = transform.localScale;
+            playerScale.x = -playerScale.x;
+            transform.localScale = playerScale;
+            Forward = !Forward;
+        }
+    }
+
+    void ResetValues()
+    {
+        anim.SetBool("Move", false);
     }
 }
