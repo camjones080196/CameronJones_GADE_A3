cc385c74a2cc1785256cd8d989c54b6ab85beec4
Cam Jones <camo080196@gmail.com>
2018-09-18 10:02:14 +0200

    1st Commit
    1st Commit

---
+++ Controlled/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float speed;
+
+    private void FixedUpdate()
+    {
+        float moveHoriz = Input.GetAxis("Horizontal");
+        float moveVert = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHoriz, moveVert, 0);
+        transform.position += movement * speed * Time.deltaTime;
+        
+    }
+}
---
+++ Controlled/Assets/Scripts/Triggers/EnableComputer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableComputer : DialogueTrigger {
+
+    public int id;
+
+    public override void FireTrigger()
+    {
+        if (triggerOnce && triggered)
+        {
+            return;
+        }
+        triggered = true;
+
+        GameObject.FindGameObjectWithTag("Computer").GetComponent<DialogueWithTrigger>().enabled = true;
+        GameObject.FindGameObjectWithTag("Trigger2").GetComponent<DialogueEngine>().ChangeDialogueToID(id, true);
+    }
+}
