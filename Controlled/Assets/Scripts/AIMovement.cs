1687050ac80359685d7d0a55ef15edc7f1dea3d2
Cam Jones <camo080196@gmail.com>
2018-09-20 13:46:41 +0200

    4th Commit
    4th Commit

---
+++ Controlled/Assets/Scripts/AIMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AIMovement : MonoBehaviour {
+
+    public bool move = false;
+    public float speed;
+
+    public Queue<Vector2> points = new Queue<Vector2>();
+
+    public abstract void startMovement();
+    
+        
+    
+}
---
+++ Controlled/Assets/Scripts/SheilaMovement.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheilaMovement : AIMovement
+{
+    public GameObject[] targets;
+    Vector3 newPos;
+
+
+    private void Start()
+    {
+        for (int k = 0; k < targets.Length; k++)
+        {
+            points.Enqueue(targets[k].transform.position);
+        }
+
+        newPos = points.Peek();
+    }
+
+    private void Update()
+    { 
+        if(points.Count > 0)
+        {
+            if (this.transform.position == newPos)
+            {
+                points.Dequeue();
+                if(points.Count > 0)
+                {
+                    newPos = points.Peek();
+                }
+                
+            }
+            else
+            {
+                this.transform.position = Vector3.MoveTowards(this.transform.position, newPos, speed * Time.deltaTime);
+            }
+        }
+    }
+
+    public override void startMovement()
+    {
+         
+
+
+
+         /*while (points.Count > 0)
+         {
+             Debug.Log(points.Peek());
+             Vector2 newPos = points.Peek();
+             this.transform.position = Vector2.MoveTowards(this.transform.position, newPos, speed * Time.deltaTime);
+             Debug.Log("Moving");
+             points.Dequeue();
+         }*/
+
+        
+    }
+}
