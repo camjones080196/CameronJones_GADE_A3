e45ae906e5ae369d5b50b5658b5fa8acb71bd93e
Cam Jones <camo080196@gmail.com>
2018-09-24 12:57:18 +0200

    7th Commit
    7th Commit

---
+++ Controlled/Assets/Scripts/SoundScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundScript : MonoBehaviour {
+
+    public AudioClip MusicClip;
+    public AudioClip MusicClip2;
+    public AudioSource MusicSource;
+    public AudioSource MusicSource2;
+
+    // Use this for initialization
+    void Start () {
+        MusicSource.clip = MusicClip;
+        MusicSource2.clip = MusicClip2;
+        MusicSource.Play();
+        MusicSource2.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
